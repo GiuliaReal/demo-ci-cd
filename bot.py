bc80e21999580159122012f3cc1c9ddb1899f2fb
GiuliaReal <giulia.real@botcity.dev>
2026-04-13 08:40:48 -0300

    update bot.py file to remove linters and bandit alerts

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 # Import for integration with BotCity Maestro SDK
-from botcity.maestro import *
-
+from botcity.maestro import (  # noqa: E402
+    BotMaestroSDK,
+    AlertType,
+    AutomationTaskFinishStatus,
+)
 
 # Disable errors if we are not connected to Maestro
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
@@ -10,22 +13,23 @@ def main():
     # Runner passes the server url, the id of the task being executed,
     # the access token and the parameters that this task receives (when applicable).
     maestro = BotMaestroSDK.from_sys_args()
-    ## Fetch the BotExecution with details from the task, including parameters
+    # Fetch the BotExecution with details from the task, including parameters
     execution = maestro.get_execution()
 
-    print(f"Task ID is: {execution.task_id}")
-    print(f"Task Parameters are: {execution.parameters}")
+    print(f"Task ID is: {execution.task_id}")  # noqa: T201
+    print(f"Task Parameters are: {execution.parameters}")  # noqa: T201
 
     maestro.alert(execution.task_id, "Example Bot started!", AlertType.INFO)
 
-    # Here you can call you main.py file or implement your bot's logic
+    # Here you can call your main.py file or implement your bot's logic
 
     # Uncomment to mark this task as finished on BotMaestro
     maestro.finish_task(
         task_id=execution.task_id,
         status=AutomationTaskFinishStatus.SUCCESS,
-        message="Congratulations on finishing first task with success. 🎉"
+        message="Congratulations on finishing first task with success. 🎉",
     )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
